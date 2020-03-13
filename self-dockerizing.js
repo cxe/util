ad52ea30b24a9954731a66658290acb6db4f0f14
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-03-13 17:08:55 +0100

    robustify exit before bash runs into js-code

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -1,12 +1,10 @@
-//usr/bin/env docker run --rm --init -it -p 8080:8080 -v "$PWD":"$PWD" -w "$PWD" node:latest node "$PWD/${0#./}" && exit
+//usr/bin/env docker run --rm --init -it -p 8080:8080 -v "$PWD":"$PWD" -w "$PWD" node:latest ${1:-node "$PWD/${0#./}"}; exit;
 
 const { log } = console;
 
-const server = require('http').createServer((req, res) => {
+require('http').createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
     log(res.statusCode, req.url);
-}).listen(8080, () => {
-    log(`Server running at http://localhost:8080/`);
-});
+}).listen(8080, _ => log(`Server running at http://localhost:8080/`));
