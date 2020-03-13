17147b6507da9dbf99324e1082b6ea679fadde57
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-03-13 16:28:28 +0100

    expose port and run sample server

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -1,3 +1,14 @@
-//usr/bin/env docker run -it -v "$PWD":"$PWD" -w "$PWD" node:latest ${1:-node "$PWD/${0#./}"} && exit
+//usr/bin/env docker run --rm -it -p ${PORT:=80}:$PORT -e "PORT=$PORT" -v "$PWD":"$PWD" -w "$PWD" node:latest node "$PWD/${0#./}" && exit
 
-console.log(process.env, process.versions);
+const { log } = console;
+const { PORT = 80 } = process.env;
+
+const server = require('http').createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(PORT, () => {
+    log(`Server running at http://localhost:${PORT}/`);
+});
