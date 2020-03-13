076ce12270504ea8e62fa3ca6d61959a88511025
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-03-13 16:42:32 +0100

    add --init for proper SIG processing

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -1,14 +1,12 @@
-//usr/bin/env docker run --rm -it -p ${PORT:=80}:$PORT -e "PORT=$PORT" -v "$PWD":"$PWD" -w "$PWD" node:latest node "$PWD/${0#./}" && exit
+//usr/bin/env docker run --rm --init -it -p 8080:8080 -v "$PWD":"$PWD" -w "$PWD" node:latest node "$PWD/${0#./}" && exit
 
 const { log } = console;
-const { PORT = 80 } = process.env;
 
 const server = require('http').createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-});
-
-server.listen(PORT, () => {
-    log(`Server running at http://localhost:${PORT}/`);
+    log(res.statusCode, req.url);
+}).listen(8080, () => {
+    log(`Server running at http://localhost:8080/`);
 });
