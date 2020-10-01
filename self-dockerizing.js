460e840199c679c3f1ed55438bb4c8f941bf48ea
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-10-01 11:48:45 +0200

    Usage

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -1,10 +1,11 @@
 //usr/bin/env docker run --rm --init -it -p 8080:8080 -v "$PWD":"$PWD" -w "$PWD" node:latest ${1:-node "$PWD/${0#./}"}; exit;
 
-const { log } = console;
-
+/**
+ * @use: make this file runnable `chmod +x self-dockerizing.js` then you can just start it `./self-dockerizing.js` and it will run in docker under the same path
+ */
 require('http').createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-    log(res.statusCode, req.url);
-}).listen(8080, _ => log(`Server running at http://localhost:8080/`));
+    console.info(res.statusCode, req.url);
+}).listen(8080, _ => console.info(`Server running at http://localhost:8080/`));
