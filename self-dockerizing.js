07fb84dc5bd449b04995e9bc17605fc3de7862ab
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-10-01 12:00:34 +0200

    Update self-dockerizing.js

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -1,7 +1,9 @@
 //usr/bin/env docker run --rm --init -it -p 8080:8080 -v "$PWD":"$PWD" -w "$PWD" node:latest ${1:-node "$PWD/${0#./}"}; exit;
 
 /**
- * @use: make this file runnable `chmod +x self-dockerizing.js` then you can just start it `./self-dockerizing.js` and it will run in docker under the same path
+ * @use:
+ *   Make this file runnable `chmod +x self-dockerizing.js` then you can just start it `./self-dockerizing.js` and it will run in docker under the same path.
+ *   As this is an interactive docker terminal you can also proxy any other command through docker e.g. `./run ls -la` 
  */
 require('http').createServer((req, res) => {
     res.statusCode = 200;
