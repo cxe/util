d6914c4d9663738eced4af8ceac1eb6ff4764aff
Ian Carter <57629805+cxe@users.noreply.github.com>
2020-10-06 11:49:54 +0200

    Update self-dockerizing.js

--- self-dockerizing.js
+++ self-dockerizing.js
@@ -4,8 +4,8 @@
  * @use:
  *   No need to install npm or node locally - all you need is `docker` to run this dockerized node service from a single file due to the bash-js-comment at the top.
  *   Make this file runnable `chmod +x self-dockerizing.js` then you can just start it `./self-dockerizing.js` and it will run in docker under the same path.
- *   As this is an interactive docker terminal you can also proxy any other command through docker e.g. `./run ls -la`
- *   To run the same file locally use `node run`.
+ *   As this is an interactive docker terminal you can also proxy any other command through docker e.g. `./self-dockerizing.js bash`
+ *   To run the same file locally use `node self-dockerizing.js`.
  */
 require('http').createServer((req, res) => {
     res.statusCode = 200;
