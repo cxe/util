cc23bfd6c0dbb43c24d843c3e2610cb55f2bd799
Ian Carter <57629805+cxe@users.noreply.github.com>
2021-05-02 00:04:30 +0200

    Update bash-utility-functions.sh

--- bash-utility-functions.sh
+++ bash-utility-functions.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env sh
 
+__DIRNAME="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echorun(){
+  cmd="$1"
+  args="${@:2}"
+  echo -e "\033[1;36m${cmd}\033[0;36m ${args}\033[0m"
+  [ -z $DRY ] && $cmd $args
+}
+
 # @usage FOO="$(findUpwardFile foo.txt)"
-findUpward() {
+readUpwardFile() {
     echo "$(f=/$1; d="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"; while [ "$d" != "/" ]; do [ -f "$d$f" ] && echo "$d$f"; d="$(dirname "$d")"; done)"
 }
 
 # @usage package_json version
 package_json() {
-    local file="$(findUpward package.json)"
+    local file="$(readUpwardFile package.json)"
     node -e "process.stdout.write('$1'.split('.').reduce((p,c)=>p && p[c], require('${file}')))"
 }
