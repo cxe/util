46a340d2108a6aa9a61e067fecdb7021292dfe2b
Ian Carter <57629805+cxe@users.noreply.github.com>
2023-05-23 08:47:52 +0200

    Update bash-utility-functions.sh

--- bash-utility-functions.sh
+++ bash-utility-functions.sh
@@ -19,3 +19,32 @@ package_json() {
     local file="$(readUpwardFile package.json)"
     node -e "process.stdout.write('$1'.split('.').reduce((p,c)=>p && p[c], require('${file}')))"
 }
+
+parseArgs(){
+      # extract options and flags
+      for arg in $@ ; do
+         if [[ "$arg" = -* ]]; then
+            if [ "$arg" == -- ]; then
+               shift; break;
+            elif [[ "$arg" = --* ]]; then
+               arg="${arg:2}"
+               [[ "$arg" = *=* ]] && {
+                  varname="option_${arg%=*}"
+                  declare $varname="${arg#*=}"
+               } || {
+                  varname="option_${arg}"
+                  declare $varname="1"
+               }
+            else
+               arg="${arg:1}"
+               for (( i=0; i<${#arg}; i++ )); do
+                  varname="flag_${arg:$i:1}"
+                  declare $varname="1"
+               done
+            fi
+            shift
+         else
+            break
+         fi
+      done
+}
