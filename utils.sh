7363faf6fd5a2359d44b280655266ce3dcdbfe3d
Ian Carter <57629805+cxe@users.noreply.github.com>
2024-05-23 22:47:55 +0200

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,3 +1,20 @@
+#!/usr/bin/env bash
+
+# @usage: `dir_containing [--stop=<dir>] <sub-path-to-find>`
+# @params: 1) (sub-)filename to find, 2) starting-dir (defaults to PWD)
+# @options: --stop=<dir> directory where to stop searching (default: <root>)
+# @return: directory-path (where the (sub-)filename was found),
+#          otherwise empty string (along with a non-zero errorcode)
+# @throws: #400 if called without parameters, #404 if not found
+# @alias:  upfind, find_upward
+dir_containing() {
+    local stop=""; [[ "$1" = --stop=* ]] && { stop="$( realpath "${1/--stop=/}" )"; shift; }
+    [ "$1" ] || return 400
+    local d="$( realpath "${2:-"$PWD"}" )"
+    while [ "$d" ] && [ "$d" != "$stop" ] && [ ! -e "$d/$1" ]; do d="${d%/*}"; done
+    [ -e "$d/$1" ] && echo "${d:-/}" || return 404
+}
+
 args(){
     declare -agx args=()
     declare -Agx opts=()
@@ -17,3 +34,5 @@ args(){
     set -- ${args[@]}
 }
 
+# if not sourced and called with args
+(return 0 2>/dev/null) || { [[ $# -gt 0 ]] && [ "$( type -t $1 )" == function ] && "$@" }
