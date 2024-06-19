1f26ccb67ed01bc963b8b64890b96c2030f01cac
Ian Carter <57629805+cxe@users.noreply.github.com>
2024-06-19 11:59:39 +0200

    add is_num

--- utils.sh
+++ utils.sh
@@ -15,6 +15,26 @@ dir_containing() {
     [ -e "$d/$1" ] && echo "${d:-/}" || return 404
 }
 
+is_num(){
+    export is_num="${1}"; shift
+    local sign="?([+-])"
+    local digit="[0123456789]" # [0-9] and [[:digit:]] can be locale-dependent
+    local deci=".+($digit)"
+    local frac="?($deci)"
+    for type; do
+        case "${type,,}" in
+            int|integer) frac="";;
+            uint) frac=""; sign="";;
+            unsigned) sign="";;
+            float|real|decimal) frac="$deci";;
+            pos|positive) sign="?(+)";;
+            neg|negative) sign="-";;
+        esac
+    done
+    local match="$sign+($digit)${frac:+$frac|$sign$deci}"
+    [[ "$is_num" == +($match) ]] || return 2
+}
+
 args(){
     declare -agx args=()
     declare -Agx opts=()
