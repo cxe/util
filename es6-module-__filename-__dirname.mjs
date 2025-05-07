8aa5a76f538724db00d525c36b576693390162c8
Ian Carter <ian.carter@innosystec.de>
2025-05-07 21:38:40 +0200

    0.3.0

--- es6-module-__filename-__dirname.mjs
+++
@@ -1,3 +0,0 @@
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = __filename.substring(0, __filename.lastIndexOf('/'));
--- freezeDeep.js
+++
@@ -1,4 +0,0 @@
-const freezeDeep = o => {
-  if (o && (typeof o === 'function' || typeof o === 'object') && !Object.isFrozen(o)) Object.freeze(o).map(prop => freezeDeep(prop));
-  return o;
-};
