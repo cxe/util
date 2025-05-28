e11f50839afb21c79297e298466aed045dab4214
Ian Carter (cxe) <57629805+cxe@users.noreply.github.com>
2025-05-28 11:59:25 +0200

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+# utils.sh v0.1.1 2025-05-28
+
+source_fetch(){ local src="$1" url="$2"; if [ ! -s "$src" ]; then mkdir -p "${src%/*}" && curl -fksSL "$url" -o "$src" || return 1; fi; source "$src"; }
