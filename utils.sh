1d2aa19e526aadf2af872a2cc3aed98f487c6e46
Ian Carter (cxe) <57629805+cxe@users.noreply.github.com>
2025-05-28 12:12:08 +0200

    0.1.2

--- utils.sh
+++ utils.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# utils.sh v0.1.1 2025-05-28
+[ "${UTILS_VERSION:-}" ] || {
+    set -a
+    UTILS_VERSION=0.1.2
+    NODE_ENV="${NODE_ENV:=development}"
+    readonly UTILS_VERSION NODE_ENV
+    set +a
 
-source_fetch(){ local src="$1" url="$2"; if [ ! -s "$src" ]; then mkdir -p "${src%/*}" && curl -fksSL "$url" -o "$src" || return 1; fi; source "$src"; }
+    source_fetch(){ local src="$1" url="$2"; if [ ! -s "$src" ]; then mkdir -p "${src%/*}" && curl -fksSL "$url" -o "$src" || return 1; fi; source "$src"; }
+}
