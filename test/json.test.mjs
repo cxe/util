8874d56f2533a2311474a1c3eea7f4295be03365
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-06-18 23:50:16 +0200

    json keywords

--- test/json.test.mjs
+++ test/json.test.mjs
@@ -2,20 +2,28 @@
 import { bash } from "../bash.mjs";
 
 const bash_json = args => bash(`./bin/json ${args}`);
-const bash_json_var = v => bash(`. "$PWD/bin/json"; test -v ${v} && echo "\$${v}" || echo undefined`).output.trim();
+const bash_json_var = v => bash(`. "$PWD/bin/json"; test -v ${v} && echo "\$${v}" || echo undefined`).output;
 
 describe("bash", () => {
   describe("json", () => {
+
     it('should expose global variables', () => {
         expect(bash_json_var("JSON_MIMETYPE")).toBe('application/json');
         expect(bash_json_var("JSON_CONFIG_COMMENTS")).toBe('');
         expect(bash_json_var("JSON_CONFIG_FILEEXT")).toContain('.json');
     });
-    it.todo('should not return an error code if valid', () => {
-        expect(bash_json('{"foo":"bar"}').errorCode).toBe(0);
+
+    it('should not return an error code if valid', () => {
+        for(const value of ['null', 'true', 'false']) { // todo: ['{"foo":"bar"}','{}', ' { } ', '\n{\n}\n', '\t{\t}\t']
+            expect(bash_json(value).errorCode).toBe(0);
+        }
     });
-    it.todo('should return an error code if invalid', () => {
-        expect(bash_json('{"foo":bar"}').errorCode).not.toBe(0);
+
+    it('should return an error code if invalid', () => {
+        for(const value of ['', 'TRUE', '...']) {
+            expect(bash_json(value).errorCode).not.toBe(0);
+        }
     });
+
   });
 });
